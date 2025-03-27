d321e93b908e79c5da632cc18f2caaa24f2df547
RishankMuddgal <muddgalrishank2004@gmail.com>
2025-03-27 09:27:09 +0530

    initial

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,15 +6,16 @@ import {Button} from '@/components/ui/button'
 function Header() {
     const location = useLocation();
   return (
-    <header className="fixed z-40 top-0 left-0 w-full p-4 ">
-        <div className="container h-16 border backdrop-blur-3xl rounded-xl  flex items-center justify-between">
+    <header className="fixed  top-0 left-0 w-full p-4 ">
+        <div className="container h-16  backdrop-blur-3xl rounded-xl  flex items-center justify-between gap-10">
             <Link to = '/' >
                 <Logo/>
             </Link>
-            <div className="flex items-center gap-2">
+            <div className="flex items-center gap-2 flex-wrap justify-end">
                 {location.pathname !== '/login' && (
                     <Button asChild 
-                        variant='ghost'>
+                        variant='ghost'
+                        >
                             <Link to = '/login'>Sign In !</Link>
                             
                     </Button>
